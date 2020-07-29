00119be84d6a30ba7c17aa7e6415022648b14f3c
hanchao <1035710787@qq.com>
2020-07-29 15:36:38 +0800

    一

---
+++ 7.29test/7.29test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  7.29test
+//
+//  Created by edz on 2020/7/29.
+//  Copyright © 2020 edz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
